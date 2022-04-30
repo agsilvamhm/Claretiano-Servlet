a7aee85b7db025e6686af4a859c1b2a86478186b
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-04-29 21:24:56 -0300

    Finalizando

--- src/main/java/Servlets/Hello.java
+++ src/main/java/Servlets/Hello.java
@@ -1,6 +1,7 @@
 package Servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -13,8 +14,17 @@ public class Hello extends HttpServlet {
 		HttpServletRequest requisicao,
 		HttpServletResponse resposta) throws ServletException , IOException
 	{
-		
+		PrintWriter out;
+		resposta.setContentType("text/html");
+		out = resposta.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Servlets - Hello World!</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h2>Primeira página com Servlet</h2>");
+		out.println("<p>Hello, World!</p>");
+		out.println("</body>");
+		out.println("</html>");
 	}
-	
-
 }
